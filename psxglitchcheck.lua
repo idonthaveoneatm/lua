2a13cdd8abcccafb97e487e3b9fd3a2c4c65fe47
idonthaveoneatm <111616655+idonthaveoneatm@users.noreply.github.com>
2023-06-16 12:36:42 -0400

    Update psxglitchcheck.lua

--- psxglitchcheck.lua
+++ psxglitchcheck.lua
@@ -1,7 +1,9 @@
+getgenv().WaitTime = 5 --time between the two checks
+
 local failed = false
 print("started")
-local PATH = game.workspace['__MAP']:WaitForChild('Interactive'):WaitForChild('Diamond Mine Collapsed Sign')
-task.wait(10)
+local PATH = game.workspace:WaitForChild('__MAP'):WaitForChild('Interactive'):WaitForChild('Diamond Mine Collapsed Sign')
+task.wait(WaitTime)
 while true do
     if PATH then
         print("failed check 1")
@@ -10,9 +12,31 @@ while true do
     task.wait(WaitTime)
     if PATH then
         print("failed check 2")
-        print("Worked")
+        local function Hop()
+        local Servers = {}
+        local function Scrape()
+            local URL = 'https://games.roblox.com/v1/games/'..game.PlaceId..'/servers/Public?sortOrder=asc&limit=100'
+            return game.HttpService:JSONDecode(game:HttpGet(URL))
+        end
+        local function TeleportServer()
+            while task.wait(3) do
+                local sid = math.random(1, #Servers)
+                game:GetService('TeleportService'):TeleportToPlaceInstance(game.PlaceId, Servers[sid], game:GetService("Players").localPlayer)
+            end
+        end
+        local function PlaceServers()
+            local scraped = Scrape()
+            for key, index in pairs(scraped.data) do
+                if index.playing and tonumber(index.playing) <= math.abs(math.random(3,10)) then
+                    table.insert(Servers, index.id)
+                end
+            end
+            TeleportServer()
+        end
+        PlaceServers()
+    end
+    Hop()
     else
         failed = false
     end
-    ]]
 end
