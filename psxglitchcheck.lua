5694f0085c4d8d21262521145621f9949618de3e
idonthaveoneatm <111616655+idonthaveoneatm@users.noreply.github.com>
2023-06-16 12:26:14 -0400

    Update psxglitchcheck.lua

--- psxglitchcheck.lua
+++ psxglitchcheck.lua
@@ -1,7 +1,3 @@
-repeat task.wait() until game.PlaceId ~= nil
-if not game:IsLoaded() then game.Loaded:Wait() end
-repeat task.wait() until not game:GetService("Players").LocalPlayer.PlayerGui:FindFirstChild("__INTRO")
-
 local failed = false
 print("started")
 local PATH = game.workspace['__MAP']:WaitForChild('Interactive'):WaitForChild('Diamond Mine Collapsed Sign')
