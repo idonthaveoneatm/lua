733904801a28d38073d486f5b6c7697e9f200037
idonthaveoneatm <111616655+idonthaveoneatm@users.noreply.github.com>
2023-06-16 01:29:21 -0400

    Update psxglitchcheck.lua

--- psxglitchcheck.lua
+++ psxglitchcheck.lua
@@ -7,6 +7,8 @@ print("started")
 local PATH = game.Players.LocalPlayer.PlayerGui:WaitForChild("Loading").Black.BackgroundTransparency
 task.wait(10)
 while true do
+    print(PATH)
+   --[[
     if PATH ==0 then
         print("failed check 1")
         failed = true
@@ -15,31 +17,8 @@ while true do
     if PATH == 0 then
         print("failed check 2")
         print("Worked")
-        local function Hop()
-        local Servers = {}
-        local function Scrape()
-            local URL = 'https://games.roblox.com/v1/games/'..game.PlaceId..'/servers/Public?sortOrder=asc&limit=100'
-            return game.HttpService:JSONDecode(game:HttpGet(URL))
-        end
-        local function TeleportServer()
-            while task.wait(3) do
-                local sid = math.random(1, #Servers)
-                game:GetService('TeleportService'):TeleportToPlaceInstance(game.PlaceId, Servers[sid], game:GetService("Players").localPlayer)
-            end
-        end
-        local function PlaceServers()
-            local scraped = Scrape()
-            for key, index in pairs(scraped.data) do
-                if index.playing and tonumber(index.playing) <= math.abs(math.random(3,10)) then
-                    table.insert(Servers, index.id)
-                end
-            end
-            TeleportServer()
-        end
-        PlaceServers()
-    end
-    Hop()
     else
         failed = false
     end
+    ]]
 end
