6e8585f76cd4fa95c9080971371e5c2f3b84842f
idonthaveoneatm <111616655+idonthaveoneatm@users.noreply.github.com>
2024-06-24 14:22:20 -0400

    Create Map3.lua

---
+++ games/PetSimulator99/table/Map3.lua
@@ -0,0 +1,171 @@
+--[[
+
+Made by griffin
+Discord: @griffindoescooking
+Github: https://github.com/idonthaveoneatm
+
+]]--
+
+return {
+    Worlds = {
+        {
+            Name = "200 | Void Spawn",
+            TeleportPart = CFrame.new(-2850.572998046875, 14.056390762329102, -8504.443359375),
+            FarmPart = CFrame.new(-2794.620849609375, 13.841465950012207, -8503.1103515625)
+        },
+        {
+            Name = "201 | Prison Block",
+            TeleportPart = CFrame.new(-2698.324951171875, 14.385641098022461, -8507.1904296875),
+            FarmPart = CFrame.new(-2631.832763671875, 13.841465950012207, -8503.982421875)
+        },
+        {
+            Name = "202 | Prison Cafeteria",
+            TeleportPart = CFrame.new(-2535.024169921875, 14.353550910949707, -8507.630859375),
+            FarmPart = CFrame.new(-2467.959716796875, 13.841465950012207, -8504.15234375)
+        },
+        {
+            Name = "203 | Prison Yard",
+            TeleportPart = CFrame.new(-2372.123779296875, 14.317835807800293, -8506.9921875),
+            FarmPart = CFrame.new(-2305.603759765625, 14.591465950012207, -8505.9482421875)
+        },
+        {
+            Name = "204 | Prison HQ",
+            TeleportPart = CFrame.new(-2208.323974609375, 13.62100601196289, -8504.15625),
+            FarmPart = CFrame.new(-2141.937255859375, 14.591465950012207, -8505.8544921875)
+        },
+        
+        {
+            Name = "207 | Tiki Island",
+            TeleportPart = CFrame.new(-3826.1015625, 411.3309326171875, -12969.77734375),
+            FarmPart = CFrame.new(-3894.145263671875, 409.9169006347656, -12968.7373046875)
+        },
+        {
+            Name = "205 | Beach Island",
+            TeleportPart = CFrame.new(-2629.7158203125, 231.2174072265625, -12963.6396484375),
+            FarmPart = CFrame.new(-2684.145263671875, 230.8227996826172, -12964.6865234375)
+        },
+        {
+            Name = "206 | Ocean Island",
+            TeleportPart = CFrame.new(-3233.224365234375, 291.1986999511719, -12969.77734375),
+            FarmPart = CFrame.new(-3304.645263671875, 290.82281494140625, -12966.9365234375)
+        },
+        {
+            Name = "208 | Jungle Island",
+            TeleportPart = CFrame.new(-4248.77685546875, 592.3612670898438, -12942.4501953125),
+            FarmPart = CFrame.new(-4295.79541015625, 591.7673950195312, -12942.1865234375)
+        },
+        {
+            Name = "209 | Volcano Island",
+            TeleportPart = CFrame.new(-4712.11767578125, 703.6817016601562, -12955.0283203125),
+            FarmPart = CFrame.new(-4758.54541015625, 702.367431640625, -12955.38671875)
+        },
+        {
+            Name = "210 | Hacker Matrix",
+            TeleportPart = CFrame.new(-2610.189208984375, 221.7504425048828, -22098.1875),
+            FarmPart = CFrame.new(-2685.16845703125, 221.42428588867188, -22099.7421875)
+        },
+        {
+            Name = "211 | Hacker Fortress",
+            TeleportPart = CFrame.new(-2381.5166015625, 197.1315155029297, -22097.1875),
+            FarmPart = CFrame.new(-2308.0126953125, 197.35687255859375, -22097.5078125)
+        },
+        {
+            Name = "212 | Hacker Cave",
+            TeleportPart = CFrame.new(-2688.826904296875, 196.66514587402344, -21800.216796875),
+            FarmPart = CFrame.new(-2689.771484375, 197.10687255859375, -21726.46875)
+        },
+        {
+            Name = "213 | Hacker Lab",
+            TeleportPart = CFrame.new(-2988.027587890625, 195.02821350097656, -22092.69921875),
+            FarmPart = CFrame.new(-3062.242431640625, 195.48187255859375, -22094.216796875)
+        },
+        {
+            Name = "214 | Hacker Mainframe",
+            TeleportPart = CFrame.new(-2686.27978515625, 194.35491943359375, -22400.6171875),
+            FarmPart = CFrame.new(-2686.020751953125, 194.93186950683594, -22473.296875)
+        },
+    },
+    Eggs = {
+        "213 | Rusty Egg",
+        "214 | Striped Egg",
+        "215 | Cinderblocks Egg",
+        "216 | Lootbag Egg",
+        "217 | Summer Beach Egg",
+        "218 | Summer Floatie Egg",
+        "219 | Summer Melon Egg",
+        "220 | Summer Beachball Egg",
+        "221 | Summer Sun Egg",
+        "222 | Hacker Circuit Egg",
+        "223 | Hacker Wireframe Egg",
+        "224 | Hacker Crystal Egg",
+        "225 | Hacker Electric Egg",
+        "226 | Hacker Secure Egg",
+    },
+    Minigames = {},
+    VendingMachines = {
+        {
+            Name = "OPPotionsVendingMachine2",
+            Location = "204 | Prison HQ"
+        },
+        {
+            Name = "RareEnchantsVendingMachine2",
+            Location = "208 | Jungle Island"
+        },
+        {
+            Name = "BundleVendingMachine3",
+            Location = "214 | Hacker Mainframe"
+        }
+    },
+    Rewards = {
+        {
+            Name = "DailyEnchants5",
+            Location = "202 | Prison Cafeteria"
+        },
+        {
+            Name = "DailyDiamonds7",
+            Location = "205 | Beach Island"
+        },
+        {
+            Name = "DailyItems5",
+            Location = "206 | Ocean Island"
+        },
+        {
+            Name = "DailyPotions6",
+            Location = "212 | Hacker Cave"
+        }
+    },
+    OtherMachines = {
+        {
+            Name = "SuperMachine",
+            Location = "200 | Void Spawn"
+        },
+        {
+            Name = "RainbowMachine",
+            Location = "200 | Void Spawn"
+        },
+        {
+            Name = "GoldMachine",
+            Location = "200 | Void Spawn"
+        },
+        {
+            Name = "IndexMerchant",
+            Location = "200 | Void Spawn"
+        },
+        {
+            Name = "PetIndexMachine",
+            Location = "200 | Void Spawn"
+        },
+        {
+            Name = "MailboxMachine",
+            Location = "200 | Void Spawn"
+        },
+        {
+            Name = "SpinnyWheel",
+            Location = "200 | Void Spawn"
+        },
+        {
+            Name = "SummerGiftMachine",
+            Location = "205 | Beach Island"
+        },
+    }
+}
